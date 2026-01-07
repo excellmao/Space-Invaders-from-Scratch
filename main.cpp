a9c6126a13b4defbdd31b240fb8556d124aa9f33
excellmao <165754199+excellmao@users.noreply.github.com>
2026-01-07 16:26:57 +0700

    adding opengl shader for drawing

--- main.cpp
+++ main.cpp
@@ -3,22 +3,32 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#define GL_ERROR_CASE(glerror)\
-    case glerror: snprintf(error, sizeof(error), "%s", #glerror)
+#define GL_ERROR_CASE(glerror) \
+    case glerror:              \
+        snprintf(error, sizeof(error), "%s", #glerror)
 
-inline void gl_debug(const char *file, int line){
+inline void gl_debug(const char *file, int line)
+{
     GLenum err;
-    while ((err = glGetError()) != GL_NO_ERROR){
+    while ((err = glGetError()) != GL_NO_ERROR)
+    {
         char error[128];
 
         switch (err)
         {
-            GL_ERROR_CASE(GL_INVALID_ENUM); break;
-            GL_ERROR_CASE(GL_INVALID_VALUE); break;
-            GL_ERROR_CASE(GL_INVALID_OPERATION); break;
-            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION); break;
-            GL_ERROR_CASE(GL_OUT_OF_MEMORY); break;
-            default: snprintf(error, sizeof(error),"%s", "UNKNOWN_ERROR"); break;
+            GL_ERROR_CASE(GL_INVALID_ENUM);
+            break;
+            GL_ERROR_CASE(GL_INVALID_VALUE);
+            break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION);
+            break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION);
+            break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY);
+            break;
+        default:
+            snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR");
+            break;
         }
 
         fprintf(stderr, "%s - %s: %d\n", error, file, line);
@@ -27,15 +37,37 @@ inline void gl_debug(const char *file, int line){
 
 #undef GL_ERROR_CASE
 
-void validate_shader(GLuint shader, const char *file = 0){
+void validate_shader(GLuint shader, const char *file = 0)
+{
+    static const unsigned int BUFFER_SIZE = 512;
+    char buffer[BUFFER_SIZE];
+    GLsizei length = 0;
 
+    glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+    if (length > 0)
+    {
+        printf("Shader %d(%s) compile error: %s\n",
+               shader, (file ? file : ""), buffer);
+    }
 }
 
-bool validate_program(GLuint program){
+bool validate_program(GLuint program)
+{
+    static const GLsizei BUFFER_SIZE = 512;
+    GLchar buffer[BUFFER_SIZE];
+    GLsizei length = 0;
 
+    glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+    if (length > 0)
+    {
+        printf("Program %d link error: %s\n", program, buffer);
+        return false;
+    }
+    return true;
 }
 
-void error_callback(int error, const char* description)
+void error_callback(int error, const char *description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
@@ -43,19 +75,33 @@ void error_callback(int error, const char* description)
 struct Buffer
 {
     size_t width, height;
-    uint32_t* data;
+    uint32_t *data;
 };
 
-uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b){
+void buffer_clear(Buffer *buffer, uint32_t color)
+{
+    for (size_t i = 0; i < buffer->width * buffer->height; ++i)
+    {
+        buffer->data[i] = color;
+    }
+}
+
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b)
+{
     return (r << 24) | (g << 16) | (b << 8) | 255;
 }
 
-int main(int argc, char* argv[]){
-    glfwSetErrorCallback(error_callback);
-    
-    GLFWwindow* window;
+int main(int argc, char *argv[])
+{
+    const size_t buffer_width = 224;
+    const size_t buffer_height = 256;
 
-    if (!glfwInit()){
+    glfwSetErrorCallback(error_callback);
+
+    GLFWwindow *window;
+
+    if (!glfwInit())
+    {
         return -1;
     };
 
@@ -66,7 +112,8 @@ int main(int argc, char* argv[]){
 
     window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
 
-    if(!window){
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
@@ -74,7 +121,8 @@ int main(int argc, char* argv[]){
     glfwMakeContextCurrent(window);
 
     GLenum err = glewInit();
-    if(err != GLEW_OK){
+    if (err != GLEW_OK)
+    {
         fprintf(stderr, "Error initializing GLEW.\n");
         glfwTerminate();
         return -1;
@@ -84,14 +132,115 @@ int main(int argc, char* argv[]){
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[1]);
 
-    gl_debug(__FILE__,__LINE__);
+    gl_debug(__FILE__, __LINE__);
 
     printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
     printf("Renderer used: %s \n", glGetString(GL_RENDERER));
     printf("Shading Language: %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     glClearColor(1.0, 0.0, 0.0, 1.0);
-    while (!glfwWindowShouldClose(window)){
+
+    uint32_t clear_color = rgb_to_uint32(0, 128, 0);
+
+    Buffer buffer;
+    buffer.width = buffer_width;
+    buffer.height = buffer_height;
+    buffer.data = new uint32_t[buffer.width * buffer.height];
+    buffer_clear(&buffer, clear_color);
+
+    // Create texture for presenting buffer to OpenGL
+    GLuint buffer_texture;
+    glGenTextures(1, &buffer_texture);
+    glBindTexture(GL_TEXTURE_2D, buffer_texture);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGB8,
+        buffer.width, buffer.height, 0,
+        GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    GLuint fullscreen_triangle_vao;
+    glGenVertexArrays(1, &fullscreen_triangle_vao);
+    glBindVertexArray(fullscreen_triangle_vao);
+
+    static const char *vertex_shader =
+        "\n"
+        "#version 330\n"
+        "\n"
+        "noperspective out vec2 TexCoord;\n"
+        "\n"
+        "void main(void){"
+        "\n"
+        "   TexCoord.x = (gl_VertexID == 2)? 2.0: 0.0;\n"
+        "   TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
+        "   \n"
+        "   gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
+        "}\n";
+
+    static const char *fragment_shader =
+        "\n"
+        "#version 330\n"
+        "\n"
+        "uniform sampler2D buffer;\n"
+        "noperspective in vec2 TexCoord;\n"
+        "\n"
+        "out vec3 outColor;\n"
+        "\n"
+        "void main(void){\n"
+        "    outColor = texture(buffer, TexCoord).rgb;\n"
+        "}\n";
+
+    GLuint shader_id = glCreateProgram();
+
+    // Create vertex shader
+    {
+        GLuint shader_vp = glCreateShader(GL_VERTEX_SHADER);
+
+        glShaderSource(shader_vp, 1, &vertex_shader, 0);
+        glCompileShader(shader_vp);
+        validate_shader(shader_vp, vertex_shader);
+        glAttachShader(shader_id, shader_vp);
+
+        glDeleteShader(shader_id);
+    }
+
+    {
+        GLuint shader_fp = glCreateShader(GL_FRAGMENT_SHADER);
+
+        glShaderSource(shader_fp, 1, &fragment_shader, 0);
+        glCompileShader(shader_fp);
+        validate_shader(shader_fp, fragment_shader);
+        glAttachShader(shader_id, shader_fp);
+
+        glDeleteShader(shader_fp);
+    }
+
+    glLinkProgram(shader_id);
+
+    if (!validate_program(shader_id))
+    {
+        fprintf(stderr, "Error while validating shader.\n");
+        glfwTerminate();
+        glDeleteVertexArrays(1, &fullscreen_triangle_vao);
+        delete[] buffer.data;
+        return -1;
+    }
+
+    glUseProgram(shader_id);
+
+    GLint location = glGetUniformLocation(shader_id, "buffer");
+    glUniform1i(location, 0);
+
+    //OpenGL setup
+    glDisable(GL_DEPTH_TEST);
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(fullscreen_triangle_vao);
+
+    while (!glfwWindowShouldClose(window))
+    {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
